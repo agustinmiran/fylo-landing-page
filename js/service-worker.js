7c914a73df9111fe9ebef9c14883b6fd3354f7fb
agustinmiran <agustinmirandagulfo3@gmail.com>
2023-05-30 22:27:14 -0500

    three commit

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,9 +1,9 @@
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register('/sw.js')
-        .then(function (registro) {
-            console.log('Service worker Registrado:', registro);
-        })
-        .catch(function (error) {
-            console.log('Service worker Registro fallido:', error);
-        })
+	navigator.serviceWorker.register('/sw.js')
+		.then(function (registro) {
+			console.log('Service worker Registrado:', registro);
+		})
+		.catch(function (error) {
+			console.log('Service worker Registro fallido:', error);
+		})
 }
